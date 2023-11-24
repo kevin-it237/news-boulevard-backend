14030b86a126f38b412277e25c8fd0d6259ecd49
kevin-it237 <ngaleuabel@gmail.com>
2023-11-24 16:35:58 +0100

    update github ci

--- config/database.js
+++ config/database.js
@@ -2,5 +2,5 @@ const localDB = "mongodb://localhost/newsboulevard";
 const liveDB = "mongodb+srv://newsboulevard:NTvMTujbOiba1Daj@cluster0.mej874v.mongodb.net/newsboulevard?retryWrites=true&w=majority"
 
 module.exports = {
-    database: process.env.NODE_ENV !== 'production' ? liveDB : liveDB
+    database: process.env.NODE_ENV !== 'production' ? localDB : liveDB
 }
