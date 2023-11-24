36782aa8afdb93ca0c4bd875e3a33de2ae18bfe5
kevin-it237 <ngaleuabel@gmail.com>
2023-11-24 16:25:12 +0100

    update mongoose db url

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const localDB = "mongodb://localhost/newsboulevard";
-const liveDB = "mongodb+srv://newsboulevard:yBOOzFXLBP39B0ni@cluster0.tvrqc.mongodb.net/newsboulevard?retryWrites=true&w=majority"
+const liveDB = "mongodb+srv://newsboulevard:NTvMTujbOiba1Daj@cluster0.mej874v.mongodb.net/newsboulevard?retryWrites=true&w=majority"
 
 module.exports = {
-    database: process.env.NODE_ENV !== 'production' ? localDB : liveDB
+    database: process.env.NODE_ENV !== 'production' ? liveDB : liveDB
 }
